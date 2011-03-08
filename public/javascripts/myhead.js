7f8cb0c5d800ba8edff82916f13e0d55f2b376de
HPNeo <hpneo@hotmail.com>
2011-03-07 22:12:33 -0500

    Actualizando template

--- public/javascripts/myhead.js
+++ public/javascripts/myhead.js
@@ -2,4 +2,5 @@ head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28
 head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.anchor.js');
 head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.color-min.js');
 head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.cycle.lite.1.0.min.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.viewport.mini.js');
 head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/application.js');
