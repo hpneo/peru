5c5639b85e91a6761ce1d64518265808501cf3e4
HPNeo <hpneo@hotmail.com>
2011-03-08 10:46:13 -0500

    Template actualizado

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,30 +1,44 @@
+// Background colors
+var colors = new Array();
+colors['home'] = '#EE2E24';
+colors['brand'] = '#ffcd34'; // yellow //'#EE2E24';
+colors['what'] = '#80c242';
+colors['why'] = '#7f1069'; //'#72175e';
+colors['how'] = '#0079c2'; //'#116fab';
+
+
+
 $(document).ready(function(){
+	
+	// Cufon tags
+	Cufon.replace('#home_events ul.events h3');
+	Cufon.replace('.banner .banner-links a'); 
+	Cufon.replace(".content h2");
+	Cufon.replace("#header #follow-us li.title");
+	Cufon.replace("#menu li a");
+	Cufon.replace(".content ul.section-content li h3");
 	
 	// Remove no-js from html tag
 	$('html').removeClass('no-js');
 	
 	/*
 	 * Background color change
+
 	 */
 	$("a.anchorLink").click(function(){
 		
 		if ($('body').is(':animated')) {
+			//console.log('click por aca esta animado');
 			return false;
 		}
 		
-		if ( $(this).hasClass('active') == true ) {
-			return false;
-		}
+		$("a.anchorLink").removeClass('active');
+		$(this).addClass('active');
 		
-		//var colors = new Object();
-		var colors = new Array();
-		colors['what'] = '#EE2E24';
-		colors['why'] = '#72175e';
-		colors['how'] = '#6c3e17';
-		colors['brand'] = '#116fab';
 		
 		$('body').animate({ backgroundColor: colors[$(this).attr('href').replace('#', '')] }, 1100);
-		
+		//location.hash = "#" + name ;
+	
 		return false;
 	});	
 	
@@ -36,27 +50,47 @@ $(document).ready(function(){
 	/*
 	 * Banner's slider
 	 */
-	$('#section-brand .banner-content').cycle({
+	$('.banner-content, .banner-dif').cycle({
 		timeout:4000,
 		speed:500,
 		sync: 1
 	});
-	
-	
+	/*
+	 * Colores de fondo
+	 */	
+	/*
 	$(window).bind("scroll", function(event) {
-		 
+
+		if ($('body').is(':animated')) {
+			//console.log('scroll aca se esta animado');
+			return false;
+		}
+		
 		 $(".section:in-viewport").each(function(){
 			 menu_top = $("#menu").offset().top;
 			 section_top = $(this).offset().top;
 			 
 			 
-			 if (menu_top - 130 > section_top){
+			 if (menu_top + 50 > section_top){
 				 var name = $(this).find('a.link-anchor').attr("name");
-				 //$("a.anchorLink").click();
 				 //console.log(name);
+				 if (name != "home"){
+				 if ( $('#menu a.'+name).hasClass('active') != true ){
+						$("a.anchorLink").removeClass('active');
+						$('#menu a.'+name).addClass('active');
+						
+		
+						$('body').animate({ backgroundColor: colors[$('#menu a.'+name).attr('href').replace('#', '')] }, 1100);					 				 
+				 }
+				}
+				else {
+					
+						$('body').animate({ backgroundColor: colors["home"] }, 1100);					 				 
+				}
+				
 			}			 
 		 });
-		 
+		
 	});
-
+	*/
 });
